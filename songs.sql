242e68f75a640aafef100160d7cd8f26ff4a291a
martinwnilsson <martin@martinnilsson.se>
2023-12-14 11:33:00 +0100

    First commit

---
+++ songs.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 14 dec 2023 kl 11:22
+-- Serverversion: 10.4.24-MariaDB
+-- PHP-version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `songsapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `songs`
+--
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `artist` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `embed` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumpning av Data i tabell `songs`
+--
+
+INSERT INTO `songs` (`id`, `name`, `artist`, `category`, `embed`) VALUES
+(1, 'Mary on a cross', 1, 1, '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/NmxFxBiCrL4?si=y_QUivwt8PY7J4yO\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>'),
+(2, 'Mood swings', 2, 2, '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/a8-X0tAhO-Q?si=380MLhi2iI9WMmCg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>');
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
